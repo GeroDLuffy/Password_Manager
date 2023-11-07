4261679525e740c5104e3ceaa705bfc755e8d691
GeroDLuffy <gerox2009@hotmail.com>
2023-11-07 17:04:39 -0300

    Se agrego confirmacion de ingreso de datos

--- data_loading/load_data.py
+++ data_loading/load_data.py
@@ -2,4 +2,5 @@ def load():
     platform = input('Ingrese plataforma: ')
     users = input('Ingrese usuario: ')
     password = input('Ingrese contraseña: ')
+    print('¡Se agregaron con exito los datos!')
     return platform, users, password
