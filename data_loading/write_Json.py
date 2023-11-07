665a30991f7ff28667536d6c54c161c8ea9b3cb3
GeroDLuffy <gerox2009@hotmail.com>
2023-11-07 11:38:08 -0300

    write_Json junta mismas plataformas

--- data_loading/write_Json.py
+++ data_loading/write_Json.py
@@ -4,16 +4,38 @@ import os
 file_json = r'Password_Manager/data_dicc.json'
 # Crear diccionario
 data_json = {}
-data_json['info'] = []
+
+try:
+    with open(file_json, 'r') as file:
+        data_json = json.load(file)
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    data_json = {}
+
 
 def add_data(plat, user, pssw):
-    data_json['info'].append({
-        'Plataforma: ' : plat,
-        'Usuario: ' : user,
-        'Contrasena: ' : pssw
-    })
+    if plat in data_json:
+        data_json[plat].append({
+            'Usuario: ' : user,
+            'Contrasena: ' : pssw
+        })
+    else:
+        data_json[plat] = [{
+            'Usuario: ' : user,
+            'Contrasena: ' : pssw
+        }]
+
     with open(file_json, 'w') as file:
         json.dump(data_json, file, indent=4)
+
+
+
+    # data_json['Plataforma'] = [plat]
+    # data_json['Plataforma'].append({
+    #     'Usuario: ' : user,
+    #     'Contrasena: ' : pssw
+    # })
+    # with open(file_json, 'w') as file:
+    #     json.dump(data_json, file, indent=4)
 
 
     # if os.path.exists(file_json):
