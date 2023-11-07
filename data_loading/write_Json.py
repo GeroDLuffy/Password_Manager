e77b6f2d1d896ed9ff2acf18d6431815cd358e3a
GeroDLuffy <gerox2009@hotmail.com>
2023-11-07 11:25:47 -0300

    Cambios write_Json

--- data_loading/write_Json.py
+++ data_loading/write_Json.py
@@ -1,13 +1,24 @@
 import json
+import os
+
+file_json = r'Password_Manager/data_dicc.json'
 # Crear diccionario
 data_json = {}
+data_json['info'] = []
 
 def add_data(plat, user, pssw):
-    data_json['info'] = ({
+    data_json['info'].append({
         'Plataforma: ' : plat,
         'Usuario: ' : user,
         'Contrasena: ' : pssw
     })
-
-    with open( r'Password_Manager/data_dicc.json', 'w') as file:
+    with open(file_json, 'w') as file:
         json.dump(data_json, file, indent=4)
+
+
+    # if os.path.exists(file_json):
+    #     with open(file_json, 'a') as file:
+    #         json.dump(data_json, file, indent=4)
+    # else:
+    #     with open(file_json, 'w') as file:
+    #         json.dump(data_json, file, indent=4)
