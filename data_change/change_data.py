af513164ebd53e3904a8c4953b93f602a5d83593
GeroDLuffy <gerox2009@hotmail.com>
2024-04-06 15:45:15 -0300

    Corregido y funcionando

--- data_change/change_data.py
+++ data_change/change_data.py
@@ -1,12 +1,42 @@
 import json
 from data_loading.write_Json import add_data
 
-file_json = r'Password_Manager/data_dicc.json'
+file_json = r'data_dicc.json'
 
 def modify():
     try:
         with open(file_json, 'r') as file:
             data = json.load(file)
+
+            # Busca si la plataforma ingresada existe en el diccionario
+            plat = input('Ingrese plataforma a buscar el dato: ').capitalize()
+            if plat in data:
+
+                # Busca si el usuario ingresado existe en la plataforma dentro del diccionario
+                user = input('Ingrese el usuario a modificar: ')
+
+                if user in data[plat]:
+                    new_user = input('Ingrese el nuevo usuario: ')
+
+                    # Recupera la contraseña del usuario, ya que solo queremos cambiar el usuario
+                    pssw = (data[plat][user])
+
+                    # Eliminamos el antiguo usuario
+                    del data[plat][user]
+
+                    # Añadimos la key cambiada
+                    add_data(plat, new_user, pssw)
+                    
+
+                else:
+                    print(f'El usuario {user} no existe en la plataforma {plat}')
+
+            else:
+                print(f'La plataforma {plat} no existe en el archivo {file_json}.')
+
+
+
+
 
     except FileNotFoundError:
         print(f"El archivo JSON '{file_json}' no existe.")
@@ -19,14 +49,15 @@ def modify():
     # data = {[plat]:[user]}
     # data{plat:[{user}]} = [new_user]
     # print(data)
-    plat = input('Ingrese plataforma: ').capitalize()
-    if plat in data:
-        user = input('Ingrese usuario a cambiar: ')
-        print(f'{plat}:{data[plat]}')
-        if user in data[plat]:
-            print('El usuario existe.')
-        else:
-            print(f'El usuario {user} no existe en la plataforma {plat}')
-    else:
-        print(f'La plataforma {plat} no existe en el diccionario.')
+    # plat = input('Ingrese plataforma: ').capitalize()
+    # if plat in data:
+    #     user = input('Ingrese usuario a cambiar: ')
+    #     print(f'{plat}:{data[plat]}')
+    #     if user in data[plat]:
+    #         print('El usuario existe.')
+    #     else:
+    #         print(f'El usuario {user} no existe en la plataforma {plat}')
+    # else:
+    #     print(f'La plataforma {plat} no existe en el diccionario.')
+
 
