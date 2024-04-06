2b5e0be3ae79484ff73ed0fc16ef355e6c53544a
GeroDLuffy <gerox2009@hotmail.com>
2024-04-06 15:40:40 -0300

    Ajustado segun las correciones en read_Json

--- data_loading/write_Json.py
+++ data_loading/write_Json.py
@@ -14,15 +14,11 @@ except (FileNotFoundError, json.decoder.JSONDecodeError):
 
 def add_data(plat, user, pssw):
     if plat in data_json:
-        data_json[plat].append({
-            'Usuario: ' : user,
-            'Contrasena: ' : pssw
-        })
+        data_json[plat][user] = pssw
     else:
-        data_json[plat] = [{
-            'Usuario: ' : user,
-            'Contrasena: ' : pssw
-        }]
+        value = {user:pssw}
+        data_json[plat] = (value)
 
     with open(file_json, 'w') as file:
         json.dump(data_json, file, indent=4)
+
